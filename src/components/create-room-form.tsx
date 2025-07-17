306fd58d60d16b6a9029333304f1504362c0aa76
Rafhael Ferreira <rafhaelferreira796@gmail.com>
2025-07-16 23:02:37 -0300

    front end Let ask me chat

---
+++ src/components/create-room-form.tsx
@@ -0,0 +1,98 @@
+/** biome-ignore-all lint/suspicious/noConsole: <explanation> */
+import { useForm } from "react-hook-form";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "./ui/card";
+import { z } from "zod/v4";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Form, FormControl, FormField, FormItem, FormLabel } from "./ui/form";
+import { Input } from "./ui/input";
+import { Textarea } from "./ui/textarea";
+import { Button } from "./ui/button";
+import { useCreateRoom } from "@/http/use-create-room";
+
+const createRoomSchema = z.object({
+  name: z.string().min(3, { message: "Inclua no mínimo 4 caracteres" }),
+  description: z.string(),
+});
+
+type CreateRoomFormData = z.infer<typeof createRoomSchema>;
+
+export function CreateRoomForm() {
+  const { mutateAsync: createRoom } = useCreateRoom();
+
+  const createRoomForm = useForm<CreateRoomFormData>({
+    resolver: zodResolver(createRoomSchema),
+    defaultValues: {
+      name: "",
+      description: "",
+    },
+  });
+
+  // biome-ignore lint/correctness/noUnusedVariables: sssssssssssss
+  async function handleCreateRoom({ name, description }: CreateRoomFormData) {
+    await createRoom({ name, description });
+    await createRoomForm.reset();
+  }
+
+  return (
+    <Card>
+      <CardHeader>
+        <CardTitle>Criar sala</CardTitle>
+        <CardDescription>
+          Crie uma nova sala para começa a fazer perguntas e receber respostas
+          da I.A
+        </CardDescription>
+      </CardHeader>
+      <CardContent>
+        <Form {...createRoomForm}>
+          <form
+            onSubmit={createRoomForm.handleSubmit(handleCreateRoom)}
+            className="flex flex-col gap-4"
+          >
+            <FormField
+              control={createRoomForm.control}
+              name="name"
+              render={({ field }) => {
+                return (
+                  <FormItem>
+                    <FormLabel>Nome da sala</FormLabel>
+                    <FormControl>
+                      <Input
+                        {...field}
+                        placeholder="Digite o nome da sala....."
+                      />
+                    </FormControl>
+                  </FormItem>
+                );
+              }}
+            />
+
+            <FormField
+              control={createRoomForm.control}
+              name="description"
+              render={({ field }) => {
+                return (
+                  <FormItem>
+                    <FormLabel>Nome da sala</FormLabel>
+                    <FormControl>
+                      <Textarea {...field} />
+                    </FormControl>
+                  </FormItem>
+                );
+              }}
+            />
+
+            <Button type="submit" className="w-full">
+              Criar sala
+            </Button>
+          </form>
+        </Form>
+      </CardContent>
+    </Card>
+  );
+}
---
+++ src/pages/create-room.tsx
@@ -0,0 +1,16 @@
+import { CreateRoomForm } from "@/components/create-room-form";
+import { RoomList } from "@/components/room-list";
+
+export function CreateRoom() {
+  return (
+    <div className="min-h-screen px-4  p-8">
+      <div className="mx-auto max-w-4xl">
+        <div className="grid grid-cols-2 items-start gap-8">
+          <RoomList />
+          <CreateRoomForm />
+          <div />
+        </div>
+      </div>
+    </div>
+  );
+}
